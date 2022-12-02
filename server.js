cfcc658409b7ce7d20d19063ee688ffe59b93fcb
Z. R <zmirza7701@gmail.com>
2022-12-01 21:34:14 -0600

    added routes and finished up code

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
-const express = require('express');
-const path = require('path');
-
-const PORT = process.env.PORT || 3001;
-
+const express = require("express");
+const fs = require("fs");
+const notes = require("./db/db.json");
+const path = require("path");
 const app = express();
+var PORT = process.env.PORT || 3001;
 
-app.use(express.static('routes'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/routes/route.js'))
-);
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./db/db.json"))
+});
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
